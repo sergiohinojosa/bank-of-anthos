8b664e244d3f017e72183f07bef54df494f2feb8
Sergio Hinojosa <sergio.hinojosa@dynatrace.com>
2023-12-27 23:51:51 +0000

    Start Loadtest Int via GitOps
    Signed-off-by: Sergio Hinojosa <sergio.hinojosa@dynatrace.com>

--- extras/azdo-integration/agent_start_loadtest.sh
+++ extras/azdo-integration/agent_start_loadtest.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 start_performance_test() {
-    cd $(System.ArtifactsDirectory)/$(Release.PrimaryArtifactSourceAlias)/extras/jmeter
+    #cd $(System.ArtifactsDirectory)/$(Release.PrimaryArtifactSourceAlias)/extras/jmeter
+    cd ../extras/jmeter
     #SERVER_URL=$(kubectl get service frontend -n $(staging_namespace) | awk '{print $4}')
-    echo "Pointing to $SERVER_URL "
+    echo "Pointing to $SERVER_URL with VirtualUsers $VU and Loops $LOOPS"
     # using default for now
-    /home/daniel_braaf/jmeter/apache-jmeter-5.5/bin/jmeter -SERVER_URL=$SERVER_URL -JVUCount=10 -JLoopCount=30 -n -t Test_Banking_Process.jmx -l testreport.jtl
+    /home/daniel_braaf/jmeter/apache-jmeter-5.5/bin/jmeter -SERVER_URL=$(SERVER_URL) -JVUCount=$(VU) -JLoopCount=$(LOOPS) -n -t Test_Banking_Process.jmx -l testreport.jtl
 }
 
 start_timestamp=$(date '+%F %H:%M:00')
