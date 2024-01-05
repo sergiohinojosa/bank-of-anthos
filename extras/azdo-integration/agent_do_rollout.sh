32aa3c4a5ecf6cdad228d1f5189c1b05c3531024
Sergio Hinojosa <sergio.hinojosa@dynatrace.com>
2024-01-05 14:57:14 +0000

    releases parameterized
    Signed-off-by: Sergio Hinojosa <sergio.hinojosa@dynatrace.com>

--- extras/azdo-integration/agent_do_rollout.sh
+++ extras/azdo-integration/agent_do_rollout.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 setDefaultValues() {
+    # RELEASE_RELEASENAME is an Azdo Pipeline variable
+    if [ -z "$RELEASE_RELEASENAME" ]; then
+        echo "\$RELEASE_RELEASENAME is empty"
+        RELEASE_RELEASENAME="Release-000"
+    fi
     # Default Variabes
     REPOSITORY="gcr.io/sales-engineering-emea/bank-of-anthos"
     VERSION="latest"
@@ -12,10 +17,18 @@ setDefaultValues() {
     NAMESPACE=${ENVIRONMENT}-${APPLICATION}
     YAMLFILE=$(date '+%Y-%m-%d_%H_%M_%S').yaml
     RESET_DB=false
+
+    # Release Info from AzDo
+    DT_RELEASE_VERSION=$VERSION
+    DT_RELEASE_BUILD_VERSION=$RELEASE_RELEASENAME
+
+    # RELEASE ID FROM AZDO
+    #RELEASE_RELEASEID=387
+    #RELEASE_RELEASENAME=Release-386
 }
 
 exportVariables() {
-    # Default Variabes
+    # Export variables so they are available in the command 'envsubst'
     export REPOSITORY=$REPOSITORY
     export VERSION=$VERSION
     export JVM_OPTS=$JVM_OPTS
@@ -24,12 +37,14 @@ exportVariables() {
     export APPLICATION=$APPLICATION
     export ENVIRONMENT=$ENVIRONMENT
     export NAMESPACE=${ENVIRONMENT}-${APPLICATION}
+    export DT_RELEASE_VERSION=$VERSION
+    export DT_RELEASE_BUILD_VERSION=$DT_RELEASE_BUILD_VERSION
 }
 
 printOutput() {
-    echo  ""
-    echo -e  "\tApplying  Deployment configuration with the following variables:"
-    echo  ""
+    echo ""
+    echo -e "\tApplying  Deployment configuration with the following variables:"
+    echo ""
     echo -e "\tREPOSITORY\t\t\t$REPOSITORY"
     echo -e "\tVERSION\t\t\t\t$VERSION"
     echo -e "\tJVM_OPTS\t\t\t$JVM_OPTS"
@@ -38,7 +53,8 @@ printOutput() {
     echo -e "\timagePullPolicy\t\t\t$imagePullPolicy"
     echo -e "\tAPPLICATION\t\t\t$APPLICATION"
     echo -e "\tENVIRONMENT\t\t\t$ENVIRONMENT"
-    echo -e "\tNAMESPACE\t\t\t$NAMESPACE"
+    echo -e "\tDT_RELEASE_VERSION\t\t$DT_RELEASE_VERSION"
+    echo -e "\tDT_RELEASE_BUILD_VERSION\t$DT_RELEASE_BUILD_VERSION"
     echo -e "\tYAMLFILE\t\t\t$YAMLFILE can be found under 'gen' folder"
 
 }
@@ -66,7 +82,7 @@ calculateVersion() {
         VERSION="1.0.2"
         ;;
     esac
-     echo "The hour is $h and the Version selected is $VERSION"
+    echo "The hour is $h and the Version selected is $VERSION"
 }
 
 printDeployments() {
@@ -92,21 +108,20 @@ rolloutDeployments() {
     kubectl wait --for=condition=Ready --timeout=300s --all pods --namespace $NAMESPACE || true
 }
 
-resetDatabase(){
-    if $RESET_DB ; then
+resetDatabase() {
+    if $RESET_DB; then
         echo "Resetting database, stateful pods will be recycled"
         kubectl delete pod -n $NAMESPACE accounts-db-0 ledger-db-0
-    else 
+    else
         echo "No database will be resetted"
     fi
 }
 
-
-applyDeploymentChange() {    
+applyDeploymentChange() {
     printOutput
-    
+
     resetDatabase
-    
+
     #envsubst <cluster/deploy.yaml | deployment-dev.yaml
     # Put in a generated file for logging.
     envsubst <deployment.yaml >gen/$YAMLFILE
