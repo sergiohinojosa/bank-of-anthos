f11b8c49d495d79d14097b7873e73b43057c5b12
danatrace <89346509+danatrace@users.noreply.github.com>
2024-01-09 11:15:53 -0800

    Update agent_start_loadtest.sh

--- extras/azdo-integration/agent_start_loadtest.sh
+++ extras/azdo-integration/agent_start_loadtest.sh
@@ -38,11 +38,42 @@ res=$(curl -X 'POST' \
   -d '{
          "params": {
             "event_type": "START_TEST",
+            "start_time": "$start_timestamp",
             "Release": $(RELEASE_RELEASEID),
             "Pipelineurl": "$(RELEASE_RELEASEWEBURL)",
             "stage": "$(ENVIRONMENT)",
             "Repository": "$(REPOSITORY)",
             "Release_Version": "$(DT_RELEASE_VERSION)",
+            "Application": "$(APPLICATION)",
+            "Namespace": "$(NAMESPACE)",
+            "Build_Version": "$(DT_RELEASE_BUILD_VERSION)"            
+         }
+         }')
+id=$(echo $res | jq -r '.id')
+echo $id
+while [[ $(get_wf_status) == "RUNNING" ]]; do
+sleep 10
+done
+
+}
+
+stop_test_wf()
+{
+create_token
+res=$(curl -X 'POST' \
+  "$(dt_tenant_url)/platform/automation/v1/workflows/$(dt_event_wf)/run" \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -H "authorization: Bearer $(echo $result_dyna)" \
+  -d '{
+         "params": {
+            "event_type": "END_TEST",
+            "stop_time": "$stop_timestamp",
+            "Release": $(Release.ReleaseId),
+            "Pipelineurl": "$(Release.ReleaseWebURL)",
+            "stage": "$(ENVIRONMENT)",
+            "Repository": "$(REPOSITORY)",
+            "Release Version": "$(DT_RELEASE_VERSION)",
             "Application": "$(APPLICATION)",
             "Namespace": "$(NAMESPACE)",
             "Build_Version": "$(DT_RELEASE_BUILD_VERSION)"            
@@ -62,7 +93,7 @@ start_performance_test() {
 
     echo "Pointing to $SERVER_URL with VirtualUsers $VU and Loops $LOOPS"
     echo "Loading Loadtest $JMX_FILE"
-    start_test_wf
+    
 
     /opt/jmeter/apache-jmeter-5.5/bin/jmeter -n -t $JMX_FILE -JSERVER_URL=$SERVER_URL -JVUCount=$VU -JLoopCount=$LOOPS  -l testreport.jtl
 }
@@ -70,10 +101,12 @@ start_performance_test() {
 start_timestamp=$(date '+%F %H:%M:00')
 echo $start_timestamp
 echo "##vso[task.setvariable variable=start_timestamp]$start_timestamp"
+start_test_wf
 start_performance_test
 stop_timestamp=$(date '+%F %H:%M:00')
 echo $stop_timestamp
 echo "##vso[task.setvariable variable=stop_timestamp]$stop_timestamp"
+stop_test_wf
 
 
 
